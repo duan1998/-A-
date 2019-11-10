3264b0c795d842504472a4dfd815d096fae13d29
1813932878@qq.com <1813932878@qq.com>
2019-11-10 09:50:29 +0800

    进度：增加了旋转控制

---
+++ Assets/CameraCtrl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    public void RotateViewClockwise()
+    {
+        transform.Rotate(Vector3.up ,- 90f,Space.World);
+    }
+    public void RotateViewAnticlockwise()
+    {
+        transform.Rotate(Vector3.up, 90f, Space.World);
+    }
+}
